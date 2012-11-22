c526cc2055f2c69f40bb94b1c2c2d81f56e54594
Marc Schleeweiß <mschleeweiss@gmail.com>
2012-11-22 20:52:39 +0100

    Fix

--- src/com/boardgames/nmm/Position.java
+++ src/com/boardgames/nmm/Position.java
@@ -1,5 +1,6 @@
 package com.boardgames.nmm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Position {
@@ -7,6 +8,10 @@ public class Position {
     private List<Position> _neighbors;
 
     private Stone _stone = null;
+    
+    public Position() {
+    	_neighbors = new ArrayList<Position>();
+    }
 
     public void setNeighbors(Position... neighbors) {
     	for (Position p : neighbors) {
